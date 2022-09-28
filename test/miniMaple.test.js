0721fe531280c86ffe7c231fb37c4ffc7e23102a
Flyagin ID <flyagin00@mail.ru>
2022-09-28 19:22:57 +0300

    added tests

--- test/miniMaple.test.js
+++ test/miniMaple.test.js
@@ -1,5 +1,40 @@
 import {MiniMaple} from "../src/miniMaple";
 
-test('it fails', () => {
-    expect(false).toBeTruthy();
+test('differentiation simple term', () => {
+    const testMaple = new MiniMaple();
+
+    testFunc = '2*x';
+    expect(testMaple.diffFunc(testFunc, x)).toBe('2');
+    testFunc = '4*x^2';
+    expect(testMaple.diffFunc(testFunc, x)).toBe('8*x');
+    testFunc = '-3*x^3';
+    expect(testMaple.diffFunc(testFunc, x)).toBe('-9*x^2');
+    testFunc = 'x^4';
+    expect(testMaple.diffFunc(testFunc, x)).toBe('4*x^3');
+});
+
+test('differentiation term without variable', () => {
+    testFunc = '10';
+    expect(testMaple.diffFunc(testFunc, x)).toBe('0');
+});
+
+test('differentiation on other variable', () => {
+    testFunc = '4*x';
+    expect(testMaple.diffFunc(testFunc, y)).toBe('0');
+    testFunc = '2*x^3';
+    expect(testMaple.diffFunc(testFunc, y)).toBe('0');
+});
+
+test('differentiation on two terms with one variable', () => {
+    testFunc = '4*x^2+2*x';
+    expect(testMaple.diffFunc(testFunc, x)).toBe('8*x+2');
+    testFunc = '2*x^3-3';
+    expect(testMaple.diffFunc(testFunc, x)).toBe('6*x^2');
+    testFunc = '-x^3+10*x^2';
+    expect(testMaple.diffFunc(testFunc, x)).toBe('-3*x^2+20*x');
+});
+
+testFunc('differentiation on two terms with two variable', () => {
+    testFunc = '20*x^4-3*y^2';
+    expect(testMaple.diffFunc(testFunc, y)).toBe('-6*y');
 });
