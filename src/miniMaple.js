5c64155248b19fb0a17edba145e2ba59b0a29506
Flyagin ID <flyagin00@mail.ru>
2022-09-28 21:56:18 +0300

    update

--- src/miniMaple.js
+++ src/miniMaple.js
@@ -1,5 +1,13 @@
 class MiniMaple{
+    constructor(){}
 
+    regexTestString(str){
+        str = str.replace(/\s+/g, '');
+        let regex = /^((\-|\+)((\d+\*){0,1}([a-z](\^\d+){0,1})|(\d+)(\*[a-z](\^\d+){0,1}){0,1}))+$/gm;
+        if(str[0] != '-')
+            str = '+'.concat(str);
+        return regex.test(str);
+    }
 }
 
 export {MiniMaple}
